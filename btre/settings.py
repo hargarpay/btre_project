d58e4f9a65f3b8e81dedf54a5bf0346cc24ef5af
Odewale Ifeoluwa <ifeoluwa.odewale@gmail.com>
2019-03-26 12:17:37 +0100

    Work on the settings

--- btre/settings.py
+++ btre/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,5 +129,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
